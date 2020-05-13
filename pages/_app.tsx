c4fff767131c255c50979f7622688d803073cbaf
Harry Chakryan <harry@webnerds.se>
2020-05-13 22:41:08 +0300

    opengraph update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,19 @@ const App = ({ Component, pageProps }) => {
                         content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
                     }
                 ]}
+                openGraph={{
+                    title: 'Top Cryptocurrencies By Market Capitalization',
+                    images: [
+                      {
+                        url: 'https://topcryptos.today/crypto_og.png',
+                      },
+                    ],
+                    site_name: 'TopCryptos.today',
+                }}
+                twitter={{
+                    handle: '@harryChakryan',
+                    cardType: 'summary_large_image',
+                }}
             />
             <Component {...pageProps} />
         </>
